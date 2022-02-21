4a884c246ab219cdf071bb4707bf992563497d9b
asad <asadjawaid99@hotmail.com>
2022-02-21 18:19:51 -0500

    Get author details page completed

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 // get the author model:
 const Author = require('../models/author');
+const Book = require('../models/book');
+const async = require('async');
 
 // Display list of all Authors.
 exports.author_list = (req, res, next) => {
@@ -16,7 +18,31 @@ exports.author_list = (req, res, next) => {
 
 // Display detail page for a specific Author.
 exports.author_detail = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+	async.parallel(
+		{
+			author: callback => {
+				Author.findById(req.params.id).exec(callback);
+			},
+			author_books: callback => {
+				Book.find({ author: req.params.id }, 'title summary').exec(callback);
+			}
+		},
+		(error, results) => {
+			if (error) return next(error);
+
+			if (results.author === null) {
+				let error = new Error('Author not found');
+				error.status = 404;
+				return next(error);
+			}
+
+			res.render('author_detail', {
+				title: 'Author Details',
+				author: results.author,
+				author_books: results.author_books
+			});
+		}
+	);
 };
 
 // Display Author create form on GET.
